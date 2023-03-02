3dcfcd5aa0b36c8e0c4618cd700527d86a79e7e4
Adam Labus <adam.labuznik@gmail.com>
2023-03-02 13:10:17 +0100

    Refactored insert note

---
+++ database/utils/makeId.js
@@ -0,0 +1,9 @@
+export default function makeId(length) {
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+	for(let i = 0; i < characters.length; ++i){
+		result += characters.charAt(Math.floor(Math.random() * characters.length))
+	}
+
+	return result
+}
